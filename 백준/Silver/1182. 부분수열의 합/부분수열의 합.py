e9d78da7a57346892f5b9ddf7d3889b2dcadab86
chungSungMin <105360496+chungSungMin@users.noreply.github.com>
2026-04-13 06:56:38 +0900

    [Silver II] Title: 부분수열의 합, Time: 188 ms, Memory: 112184 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,23 +1,19 @@
-def dfs(value, lst, depth, N):
-    global cnt 
-    # and dpeth를 하는 이유는 최소한 원소를 1개 이상 갖는 경우에만 정답으로 처리해주기 떄문입니다.
-    if value == S and depth:
-        cnt += 1
+def dfs(depth, value):
+    global ans 
+    if value == M and depth : 
+        ans += 1
 
     for i in range(depth, N):
         value += lst[i]
-        dfs(value, lst, i+1, N)
+        dfs(i+1, value)
         value -= lst[i]
-        
+            
 
 if __name__ == "__main__" :
-    
-    N, S = map(int, input().split())
-    lst = list(map(int, input().split()))
-    lst = sorted(lst)
-    
-    cnt = 0
+    N, M = map(int, input().split())
+    lst = sorted(list(map(int, input().split())))
     value = 0
+    ans = 0
     
-    dfs(value, lst, 0, N )
-    print(cnt)
+    dfs(0, 0)
+    print(ans)
